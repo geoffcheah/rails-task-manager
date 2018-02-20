ec0ebc4c86fc6a379ce6d824fbc08c834b1f899d
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-20 13:55:21 +0000

    added ability to edit forms. Refactored code too create a partial form so as to reuse the form from new on the edit page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Read all tasks
   get 'tasks', to: 'tasks#index'
-  # Read one task details
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-
-  get 'tasks/:id', to: 'tasks#show', as: :task
   # create a task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # Read one task details
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
 end
